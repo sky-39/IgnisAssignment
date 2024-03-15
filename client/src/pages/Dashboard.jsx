e646a443eba1e434fea50aa8b138f4cea3d4de66
Akash Tiwari <akash_t@bt.iitr.ac.in>
2024-03-15 12:06:04 +0530

    client-setup-register-login-resetPassword-activation-redux

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from 'react-redux'
+
+
+const Dashboard = () => {
+
+    const { userInfo } = useSelector((state) => state.auth)
+
+
+    return (
+        <div>
+            <h1>Welcome, {userInfo.first_name} </h1>
+        </div>
+    )
+}
+
+export default Dashboard
